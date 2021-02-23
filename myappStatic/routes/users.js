22e365446f326c7600a28d452b60ea45fe99aee4
DrDue <dr.due98@gmail.com>
2021-02-23 12:26:03 +0100

    app.js

--- myappStatic/routes/users.js
+++ myappStatic/routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('about', { title: 'About' });
+  res.send('respond with a resource');
 });
 
 module.exports = router;
