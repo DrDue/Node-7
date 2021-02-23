5fa5c951d0694635aa99b898a62ee171f45d3f95
langehk <32093132+langehk@users.noreply.github.com>
2021-02-23 13:09:12 +0100

    done

--- Dynamic/routes/index.js
+++ Dynamic/routes/index.js
@@ -3,6 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+    
+  console.log(req.params); // Det er vores paramter vi søger efter, f.eks www.dkexit.eu/users  - hvor users indholdet er vores param
+  console.log(req.body); // Indholdet i vores request
+  console.log(req.url); // url f.eks /about
+  console.log(req.query); // Hvis vi sender en parameter med vores query i headeren f.eks  ?id=109
+
   res.render('index', { title: 'Express' });
   
 });
